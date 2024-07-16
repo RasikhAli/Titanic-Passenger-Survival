cf4a880c91b3c5643da03f99767bc407975e2c3d
Rasikh Ali <89584191+RasikhAli@users.noreply.github.com>
2024-07-16 19:44:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def generate_classifier_comparison_charts():
     plt.savefig('static/classifier_accuracy_trend.png')
     plt.close()
 
-# if __name__ == '__main__':
-    # app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
     # port = int(os.environ.get('PORT', 5000))
     # app.run(host='0.0.0.0', port=port)
