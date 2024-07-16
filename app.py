8e9b95466bd5d69eade0ac0e0b122e4f57b710fd
Rasikh Ali <89584191+RasikhAli@users.noreply.github.com>
2024-07-16 18:06:55 +0500

    Update app.py
    Update to use port from environment variable

--- app.py
+++ app.py
@@ -145,5 +145,6 @@ def generate_classifier_comparison_charts():
     plt.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=5000)
