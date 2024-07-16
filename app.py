8780ed9c0e39a6ade8565462066a743f7548e3b7
Rasikh Ali <89584191+RasikhAli@users.noreply.github.com>
2024-07-16 18:02:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -145,4 +145,5 @@ def generate_classifier_comparison_charts():
     plt.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
